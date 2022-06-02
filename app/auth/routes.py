aba4651c1512d270f099a8ff7db55534dd261277
MarcusOWilliams <molowilliams@gmail.com>
2022-06-02 14:59:44 +0100

    Add Error handlers

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -17,14 +17,14 @@ def login():
 
         user = User.query.filter_by(username = form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
 
         login_user(user, remember=form.remember_me.data)
 
         #once the user is loged in they are redirected to the page they tried to visit, if they came straight to the login page they are just redirected to the home page
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main.index')
 
         return redirect(next_page)
        
@@ -34,7 +34,7 @@ def login():
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
 
 @bp.route('/register', methods=['GET', 'POST'])
