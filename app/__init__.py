090fc1431831cc37f02bf7fd176abb6d4113f191
MarcusOWilliams <molowilliams@gmail.com>
2022-06-06 09:40:35 +0100

    add flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,12 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from flask_moment import Moment
 
+#The database is run using SQLAlchemy
 db = SQLAlchemy()
 
+#Allows migration of database changes
 migrate = Migrate()
 
 #For login manager
@@ -15,9 +18,14 @@ login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = ('You must be signed in to access this page.')
 
+#For password hashing
 bcrypt = Bcrypt()
 
+#For sending emails
 mail = Mail()
+
+#For nice datetime formatiing
+moment = Moment()
 
 def create_app(config_Class = Config):
     #generate the app with the specified configurations
@@ -36,6 +44,9 @@ def create_app(config_Class = Config):
 
     #add email support
     mail.init_app(app)
+
+    #Link moment for datetime 
+    moment.init_app(app)
 
     #link the bluebrints
     from app.main import bp as main_bp
