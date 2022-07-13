d049283190d210f3a01f22e090dd72ffe615c184
MarcusOWilliams <molowilliams@gmail.com>
2022-07-13 16:47:53 +0100

    wording  updates

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Linking the database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_recycle': 299,
     }
