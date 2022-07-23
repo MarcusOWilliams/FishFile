04f0007b52f4cc4202080559948e678809f761ff
MarcusOWilliams <molowilliams@gmail.com>
2022-07-23 15:33:58 +0100

    Update test_home_page.py

--- tests/functional/test_home_page.py
+++ tests/functional/test_home_page.py
@@ -1,31 +1,31 @@
 
 from flask import url_for,request
 
-def test_home_page(client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET) 
-    AND the user is not logged in
-    THEN check that the response is a redirect
-    """
+# def test_home_page(client):
+#     """
+#     GIVEN a Flask application configured for testing
+#     WHEN the '/' page is requested (GET) 
+#     AND the user is not logged in
+#     THEN check that the response is a redirect
+#     """
 
-    #check for a redirect response
-    response = client.get(url_for('/home'))
-    assert response.status_code == 302
+#     #check for a redirect response
+#     response = client.get(url_for('/home'))
+#     assert response.status_code == 302
 
-    #check the redirect goes to the login page
-    response = client.get('/', follow_redirects=True)
-    assert request.path == url_for('auth.login')
+#     #check the redirect goes to the login page
+#     response = client.get('/', follow_redirects=True)
+#     assert request.path == url_for('auth.login')
 
-def test_home_page(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET) 
-    AND the user authentication is bypassed
-    THEN check that the response is valid
-    """
-    response = test_client.get('/home')
-    assert response.status_code == 200
+# def test_home_page(test_client):
+#     """
+#     GIVEN a Flask application configured for testing
+#     WHEN the '/' page is requested (GET) 
+#     AND the user authentication is bypassed
+#     THEN check that the response is valid
+#     """
+#     response = test_client.get('/home')
+#     assert response.status_code == 200
 
-    assert b"Welcome to DanioDB" in response.data
-    assert b"incorrect" not in response.data
+#     assert b"Welcome to DanioDB" in response.data
+#     assert b"incorrect" not in response.data
