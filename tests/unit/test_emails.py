7671839a3f7369a58863f3ca845519921f288967
MarcusOWilliams <molowilliams@gmail.com>
2022-07-25 10:20:08 +0100

    Update test_emails.py

--- tests/unit/test_emails.py
+++ tests/unit/test_emails.py
@@ -1,3 +1,12 @@
 from flask_mail import Message
+from app import mail
+
 def test_reset_password_email(new_user, test_client):
-    pass
+    with mail.record_messages() as outbox:
+
+        mail.send_message(subject='testing',
+                        body='test',
+                        recipients="mw2056@bath.ac.uk")
+
+        assert len(outbox) == 1
+        assert outbox[0].subject == "testing"
