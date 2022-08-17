47442955cd7ccf22c16ef85d3761aa830e693307
MarcusOWilliams <molowilliams@gmail.com>
2022-08-17 16:50:18 +0100

    Update fishFile.py

--- fishFile.py
+++ fishFile.py
@@ -3,7 +3,7 @@
 # this file is declared as the flask_app in the env file, so when running the virtual environment, this is the file that is run
 # to Run the file activate the venv using venv\scripts\activate in the terminal (windows) then type "flask run"
 from app import create_app, db
-from app.models import Allele, Photo, User, Fish, Change, Settings, Notification, Reminder, Stock
+from app.models import Allele, Photo, User, Fish, Change, Settings, Notification, Reminder, Stock, Transgene
 
 
 app = create_app()
@@ -12,4 +12,4 @@ app = create_app()
 # this part allows you to run a python interpreter session in the context of the flask app using the 'flask shell' command
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Fish': Fish, 'Change': Change, 'Settings': Settings, 'Notification': Notification, 'Reminder':Reminder, 'Allele':Allele, 'Photo':Photo, 'Stock':Stock}
+    return {'db': db, 'User': User, 'Fish': Fish, 'Change': Change, 'Settings': Settings, 'Notification': Notification, 'Reminder':Reminder, 'Allele':Allele, 'Photo':Photo, 'Stock':Stock, 'Transgene':Transgene}
