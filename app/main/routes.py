2f061d24c29e34d3ef56b3b6f096c3f83b885890
MarcusOWilliams <molowilliams@gmail.com>
2022-09-08 16:45:26 +0100

    attempt at making user codes matching sets

--- app/main/routes.py
+++ app/main/routes.py
@@ -201,12 +201,12 @@ def search():
             new = (
                 Fish.query.select_entity_from(all_fish)
                 .join(Fish.user_code, aliased=True)
-                .filter(User.code.contains(search_dict[key]))
+                .filter(set(User.code)).issuperset(set(search_dict[key]))
 
             )
             old = (
                 Fish.query.select_entity_from(all_fish)
-                .filter(Fish.old_code.contains(search_dict[key]))
+                .filter(set(Fish.old_code).issuperset(set(search_dict[key])))
 
             )
             all_fish = new.union(old).subquery()
